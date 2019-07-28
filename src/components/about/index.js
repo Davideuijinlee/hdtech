9e30e265a2d83ef7c3e4761bc52335262fb3332d
Davideuijinlee <Davideuijinlee@gmail.com>
2019-07-27 22:14:13 -0700

    completed about section

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -3,8 +3,17 @@ import './about.scss';
 
 export default () => {
     return (
-        <div className="aboutContainer">
-            <h1>Who We Are</h1>
+        <div className="col-12 container aboutContainer">
+            <div className="row">
+                <div className="col-6 phraseContainer">
+                </div>
+                <div className="col-12 col-md-6 col-lg-6 aboutContainer">
+                <h2 className="offset-3 col-6  offset-md-0 col-md-6 col-lg-12 aboutText">ABOUT US</h2>
+                <div className="col-12 col-md-10 col-lg-8 aboutUs">
+                Located in the heart of Orange County, HD-Tech Marketing Solutions, LLC. is a full-service digital marketing agency aimed at providing businesses with a wide variety of successful and affordable marketing strategies. With a combined 12 years of Web Development and SEO experience, we have the tools to take your business to the next level utilizing services such as Google Ads, Facebook for Business, etc...Whether starting a new business or simply rebranding, we have the tools to make your business stand out above the rest.
+                </div>
+            </div>
+            </div>
         </div>
     )
 }
